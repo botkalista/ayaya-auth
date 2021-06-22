859c5b268040d678088cad93130484f3145f809b
Emily <adharaultimasheireen@gmail.com>
2021-06-22 02:15:39 +0200

    update 1.0.5

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,28 @@ import { AuthConfig } from './types/AuthConfig';
 
 let client: MongoClient;
 let usersCollection: Collection;
-
 let userRepo: UserRepository;
 
 async function init(config: AuthConfig) {
     client = await new MongoClient(config.connectionString).connect();
     usersCollection = client.db(config.dbName).collection(config.userCollection);
-
     userRepo = new UserRepository(usersCollection, config);
 }
 
+export async function disconnect(options: { force: boolean } = { force: false }) {
+    if (options.force) {
+        for (let i = 0; i < 5; i++) {
+            await new Promise(r => setTimeout(r, 1000));
+            if (client && client.isConnected()) return await client.close();
+        }
+    } else {
+        if (!client) return;
+        if (client.isConnected()) await client.close();
+    }
+}
 
 export async function registerRoutes(app, path, config: AuthConfig) {
-    init(config);
+    await init(config);
     const router = await createRouter(userRepo, config);
     app.use(path, router);
     return router;
--- test.ts
+++ test.ts
@@ -1,25 +1,63 @@
 import * as express from 'express';
 
-import { registerRoutes, authMiddleware } from './src/index';
+import { registerRoutes, authMiddleware, disconnect } from './src/index';
 
-const app = express();
+async function setupServer() {
+    const app = express();
 
-registerRoutes(app, '/account', {
-    connectionString: 'mongodb://admin:cameriera@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
-    jwtKey: 'test',
-    dbName: 'AyayaAuth',
-    userCollection: 'Users',
-    userModel: {
-        test: 123
-    },
-    routes: {
-        login: '/login',
-        registration: '/create'
-    }
-});
+    await registerRoutes(app, '/account', {
+        connectionString: 'mongodb://admin:cameriera@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+        jwtKey: 'test',
+        dbName: 'AyayaAuth',
+        userCollection: 'Users',
+        userModel: {
+            test: 123
+        },
+        routes: {
+            login: '/login',
+            registration: '/create'
+        }
+    });
+    
+    app.get('/test', authMiddleware, (req, res) => {
+        res.json({ asd: 123 });
+    })
 
-app.get('/test', authMiddleware, (req, res) => {
-    res.json({ asd: 123 });
-})
+    return app;
+}
 
-app.listen(8030);
+const fetch = require('node-fetch');
+
+function assert(expression, value) {
+    if (expression == value) return;
+    console.log('TEST ERROR: ', expression, value);
+}
+
+async function testing() {
+    const server = (await setupServer()).listen(8030);
+    const host = 'http://127.0.0.1:8030';
+
+    const response_test_no_auth = await fetch(`${host}/test`);
+    assert(response_test_no_auth.status, 401);
+
+    const response_create_account = await fetch(`${host}/account/create`, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({
+            username: 'test',
+            password: 'test'
+        })
+    });
+
+    const response_create_account_data = await response_create_account.json();
+
+    assert(response_create_account.status, 200);
+    assert(response_create_account_data, 200);
+
+    server.close();
+    disconnect({ force: true });
+    console.log('TEST FINISHED');
+}
+
+
+testing();
